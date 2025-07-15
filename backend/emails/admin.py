8e00c7b73361f4d4f3be84c843a38227839343e4
mattyray <mnraynor90@gmail.com>
2025-07-15 10:10:24 -0400

    emails/admin

--- backend/emails/admin.py
+++ backend/emails/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import EmailTemplate, EmailLog
 
-# Register your models here.
+@admin.register(EmailTemplate)
+class EmailTemplateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subject', 'is_active']
+
+@admin.register(EmailLog)
+class EmailLogAdmin(admin.ModelAdmin):
+    list_display = ['recipient_email', 'subject', 'was_sent', 'sent_at']
+    readonly_fields = ['sent_at']
+    list_filter = ['was_sent']
+    
+    def has_add_permission(self, request):
+        return False  # Emails created automatically, not manually
